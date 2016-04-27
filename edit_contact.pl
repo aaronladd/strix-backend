b03d6f9871db03524388357c633d1dc820257542
Aaron <fallenseriphan@gmail.com>
2016-04-26 18:04:10 -0600

    Update edit_contact.pl

--- edit_contact.pl
+++ edit_contact.pl
@@ -10,7 +10,7 @@ main(@ARGV);
 sub main {
 	my ($accountId)=$_[0];
 	my $email=dataBasePull(dataBaseConnection(),$accountId,0);
-	my $nagAcctPath="/usr/local/nagios/etc/accounts/$email[0][0]";
+	my $nagAcctPath="/usr/local/nagios/etc/accounts/$email->[0][0]";
 	
 	editContact($nagAcctPath, $accountId);
 }
