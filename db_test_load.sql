7f4306ce138ee887f360cfb917ba39ab97212518
Aaron <fallenseriphan@gmail.com>
2016-05-05 12:10:18 -0600

    Update db_test_load.sql

--- db_test_load.sql
+++ db_test_load.sql
@@ -189,11 +189,23 @@ use nagiUpdates;
 INSERT INTO accounts_updated(
 account_id, change_type, contacts, contacts_group, hosts
 ) VALUES (
-'1', 'E', '1', '0', '0'
+'1', 'C', '1', '1', '1'
 );
 
 INSERT INTO accounts_updated(
 account_id, change_type, contacts, contacts_group, hosts
 ) VALUES (
 '2', 'C', '1', '1', '1'
+);
+
+INSERT INTO accounts_updated(
+account_id, change_type, contacts, contacts_group, hosts
+) VALUES (
+'3', 'E', '1', '1', '1'
+);
+
+INSERT INTO accounts_updated(
+account_id, change_type, contacts, contacts_group, hosts
+) VALUES (
+'4', 'E', '1', '1', '1'
 );
