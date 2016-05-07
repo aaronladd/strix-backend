d0420175dc28ecbf958e1a0f8586088545f578fb
Aaron <fallenseriphan@gmail.com>
2016-05-06 18:44:17 -0600

    Update create_account.pl

--- file_scripts/create_account.pl
+++ file_scripts/create_account.pl
@@ -5,17 +5,26 @@ use warnings;
 use POSIX;
 use DBI;
 
+#calling the "main" subroutine and passing any arugments to it. 
 main(@ARGV);
 
+#Sub Main
+#	Arguments Account_Id
+#Pulls info from the database, sets the account path to the user's email address
+#Calls the fileCreation routine passing the newAcctPath
+#Calls the defaultContact routine passing the newAcctPath and database information.
+#Accepts all arguments passed from the command line only used the first one which should be the account id.
 sub main {
-	my $accountId=$_[0];
-	my $dataPull=dataBasePull(dataBaseConnection(),$accountId);
+	my $dataPull=dataBasePull(dataBaseConnection(),$_[0]);
 	my $newAcctPath="/usr/local/nagios/etc/accounts/$dataPull->[0][7]";
 	
-	fileCreation($newAcctPath);	
-	defaultContact($newAcctPath,$dataPull);
+	fileCreation($newAcctPath);
 }
 
+#Sub fileCreation
+#	Arguments $newAccountPath
+#Creates all necessary files in nagios configuration directory for specified account ID
+#Accepts 1 argument which should be the newAccountPath
 sub fileCreation{
 	my $newAcctPath=$_[0];
 	my @newDirectories=("contacts", "hosts");
@@ -27,11 +36,7 @@ sub fileCreation{
 	}
 		
 	foreach my $line (@newDirectories){
-		if (-e "$newAcctPath/$line") {
-			die "Directory $newAcctPath/$line  already exists.\n"
-		} else {
-			mkdir "$newAcctPath/$line";
-		}
+		mkdir "$newAcctPath/$line";
 	}
 
 	open CONTACTFILE, '>', "$newAcctPath/contacts/contacts.cfg" || die "Unable to open $newAcctPath/contacts/contacts.cfg\n";
@@ -43,28 +48,9 @@ sub fileCreation{
 	close HOSTFILE;
 }
 
-sub defaultContact {
-	my $newAcctPath=$_[0];
-	my $dataPull=$_[1];
-	my $contactFile="$newAcctPath/contacts/contacts.cfg";
-	my $contactBackupFile="$newAcctPath/contacts/contacts.bkp_cfg";
-	my @contactFields=(";contact_id 01", "contact_name", ";alias", "use", ";contactgroups", "email", ";address1", ";address2", "host_notifications_enabled 0", "service_notifications_enabled 0");
-	
-	$contactFields[1]="$contactFields[1] $dataPull->[0][1]";
-	$contactFields[3]="$contactFields[3] $dataPull->[0][3]";
-	$contactFields[5]="$contactFields[5] $dataPull->[0][7]";
-	
-	rename $contactFile, $contactBackupFile;
-		
-	open CONTACTFILE, ">$contactFile" or die $!;
-	print CONTACTFILE "define contact{\n";
-	foreach my $line (@contactFields){
-		print CONTACTFILE "\t$line\n";
-	}
-	print CONTACTFILE "}\n";
-	close CONTACTFILE;
-}
-
+#sub dataBaseConnection
+#sets the database connection information and creates the $dbh variable
+#	returns $dbh
 sub dataBaseConnection {
 	my $dsn='dbi:mysql:strixdb';
 	my $user='nagiTest';
@@ -74,37 +60,25 @@ sub dataBaseConnection {
 	return ($dbh);
 }
 
+#sub dataBasePull
+#	Arguments $dbh $accountId
+#defines the query being used
+#prepares the query against the database connection then exectues it.
+#fetches all information returned by the database and sets it to $dump
+#finishes and disconnects from the database to stay clean
+#	Returns $dump
 sub dataBasePull {
 	my $dbh=$_[0];
-	my $accountId=$_[1];
 	my ($sth, $dump, $query);
 	
-	$query="SELECT * FROM account_information WHERE account_id='$accountId'";
+	$query="SELECT * FROM account_information WHERE account_id='$_[1]'";
 	
+	print "Preparing Query";
 	$sth=$dbh->prepare($query) || die "Prepare failed: $DBI::errstr\n";
 	$sth->execute() || die "Couldn't execute query: $DBI::errstr\n";  
 	$dump=$sth->fetchall_arrayref;
+	print "Data Fetched"
 	$sth->finish();
 	$dbh->disconnect || die "Failed to disconnect\n";
 	return $dump;
 }
-
-#--accounts
-#--|--email_address
-#--|--|--contacts
-#--|--|--|--contacts.cfg
-#--|--|-----contacts_group.cfg
-#--|-----hosts
-#--|-----|--host1.cfg
-#--|-----|--host2.cfg
-#--|-----|--host3.cfg
-#--|--------host4.cfg
-#--|--email_address
-#--|--|--contacts
-#--|--|--|--contacts.cfg
-#--|--|-----contacts_group.cfg
-#--|-----hosts
-#--|-----|--host1.cfg
-#--|-----|--host2.cfg
-#--|-----|--host3.cfg
-#-----------host4.cfg
