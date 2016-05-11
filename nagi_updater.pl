ce03105c3a87534e785ba67ce862fb9f5a5a4780
Aaron <fallenseriphan@gmail.com>
2016-05-11 16:55:10 -0600

    Update nagi_updater.pl

--- nagi_updater.pl
+++ nagi_updater.pl
@@ -5,28 +5,42 @@ use warnings;
 use POSIX;
 use DBI;
 
-main(@ARGV);
+#calling the "main" subroutine.
+main();
 
+#Subroutine main
+#Pulls all rows from updates database
+#iterates through every row 
 sub main {
 	my $dataPull=dataBasePull(dataBaseConnection());
-	my $row=-1;
+	my $row=0;
 	
-	while($row<$#{$dataPull}){
+	#iterates through every row if the account is being created it calls one sub otherwise it calls the other.
+	while($row<=$#{$dataPull}){
 		$row++;
 		if($dataPull->[$row][1] eq "C"){
+			#calls accountCreation passing only the accountId
 			accountCreation($dataPull->[$row][0]);
 		} else {
+			#calls fileRebuild passing the array
 			fileRebuild($dataPull->[$row]);
 		}
 	}
 	
 }
 
+#Subroutine accountCreation
+#	Arguments $accountId
+#Routine starts the create_account script and then deletes the row
 sub accountCreation{
-	system "/home/nagios/scripting/strix-backend/file_scripts/create_account.pl $_[0] 2>&1 &";
-	#rowDelete(dataBaseConnection(),$accountId);
+	#runs the create_account script in the background passing the accountid as an argument
+	system "/home/nagios/scripting/strix-backend/file_scripts/create_account.pl $_[0] 2>&1 >> /var/log/nagios/account_creation.log &";
+	rowDelete(dataBaseConnection(),$accountId);
 }
 
+#Subroutine accountCreation
+#	Arguments $database information
+#Routine defines values from the database information then if any fields are 1 it runs the respective update script to rebuild the file then deletes the row
 sub fileRebuild{
 	my $accountId=$_[0][0];
 	my $contact=$_[0][2];
@@ -34,19 +48,22 @@ sub fileRebuild{
 	my $host=$_[0][4];
 	
 	if ($contact == 1){
-		system "/home/nagios/scripting/strix-backend/file_scripts/rebuild_contact.pl $accountId 2>&1 &";
+		system "/home/nagios/scripting/strix-backend/file_scripts/rebuild_contact.pl $accountId 2>&1 >> /var/log/nagios/contact_rebuilding.log &";
 	}
 	
 	if ($cgroup == 1){
-		system "/home/nagios/scripting/strix-backend/file_scripts/rebuild_groups.pl $accountId 2>&1 &";
+		system "/home/nagios/scripting/strix-backend/file_scripts/rebuild_groups.pl $accountId 2>&1 >> /var/log/nagios/group_rebuilding.log &";
 	}
 	
 	if ($host == 1){
-		system "/home/nagios/scripting/strix-backend/file_scripts/rebuild_host.pl $accountId 2>&1 &";
+		system "/home/nagios/scripting/strix-backend/file_scripts/rebuild_host.pl $accountId 2>&1 >> /var/log/nagios/host_rebuilding.log &";
 	}
-	#rowDelete(dataBaseConnection(),$accountId);
+	rowDelete(dataBaseConnection(),$accountId);
 }
 
+#sub dataBaseConnection
+#sets the database connection information and creates the $dbh variable
+#	returns $dbh
 sub dataBaseConnection {
 	my $dsn='dbi:mysql:nagiUpdates';
 	my $user='nagiTest';
@@ -56,9 +73,13 @@ sub dataBaseConnection {
 	return ($dbh);
 }
 
+#sub dataBasePull
+#	Arguments $dbh
+#Pulls all rows from the database
+#	Returns $dump
 sub dataBasePull {
-	my $dbh=$_[0];
 	my ($sth, $dump, $query);
+	my $dbh=$_[0];
 	
 	$query="SELECT * FROM accounts_updated";
 	
@@ -70,6 +91,10 @@ sub dataBasePull {
 	return $dump;
 }
 
+#sub rowDelete
+#	Arguments $dbh $accountId
+#Deletes teh specified row
+#	Returns $dump
 sub rowDelete {
 	my $dbh=$_[0];
 	my ($sth, $dump, $query);
