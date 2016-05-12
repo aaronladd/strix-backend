091825584eb07d51440443528f587d2969618b16
Aaron <fallenseriphan@gmail.com>
2016-05-11 18:25:57 -0600

    Update db_nagi_update_load.sql

--- db_nagi_update_load.sql
+++ db_nagi_update_load.sql
@@ -23,3 +23,135 @@ account_id, change_type, contacts, contacts_group, hosts
 ) VALUES (
 '2', 'E', '1', '1', '1'
 );
+
+INSERT INTO accounts_updated(
+account_id, change_type, contacts, contacts_group, hosts
+) VALUES (
+'1', 'E', '1', '1', '1'
+);
+
+INSERT INTO accounts_updated(
+account_id, change_type, contacts, contacts_group, hosts
+) VALUES (
+'2', 'E', '1', '1', '1'
+);
+
+INSERT INTO accounts_updated(
+account_id, change_type, contacts, contacts_group, hosts
+) VALUES (
+'1', 'E', '1', '1', '1'
+);
+
+INSERT INTO accounts_updated(
+account_id, change_type, contacts, contacts_group, hosts
+) VALUES (
+'2', 'E', '1', '1', '1'
+);
+
+INSERT INTO accounts_updated(
+account_id, change_type, contacts, contacts_group, hosts
+) VALUES (
+'1', 'E', '1', '1', '1'
+);
+
+INSERT INTO accounts_updated(
+account_id, change_type, contacts, contacts_group, hosts
+) VALUES (
+'2', 'E', '1', '1', '1'
+);
+
+INSERT INTO accounts_updated(
+account_id, change_type, contacts, contacts_group, hosts
+) VALUES (
+'1', 'E', '1', '1', '1'
+);
+
+INSERT INTO accounts_updated(
+account_id, change_type, contacts, contacts_group, hosts
+) VALUES (
+'2', 'E', '1', '1', '1'
+);
+
+INSERT INTO accounts_updated(
+account_id, change_type, contacts, contacts_group, hosts
+) VALUES (
+'1', 'E', '1', '1', '1'
+);
+
+INSERT INTO accounts_updated(
+account_id, change_type, contacts, contacts_group, hosts
+) VALUES (
+'2', 'E', '1', '1', '1'
+);
+
+INSERT INTO accounts_updated(
+account_id, change_type, contacts, contacts_group, hosts
+) VALUES (
+'1', 'E', '1', '1', '1'
+);
+
+INSERT INTO accounts_updated(
+account_id, change_type, contacts, contacts_group, hosts
+) VALUES (
+'2', 'E', '1', '1', '1'
+);
+
+INSERT INTO accounts_updated(
+account_id, change_type, contacts, contacts_group, hosts
+) VALUES (
+'1', 'E', '1', '1', '1'
+);
+
+INSERT INTO accounts_updated(
+account_id, change_type, contacts, contacts_group, hosts
+) VALUES (
+'2', 'E', '1', '1', '1'
+);
+
+INSERT INTO accounts_updated(
+account_id, change_type, contacts, contacts_group, hosts
+) VALUES (
+'1', 'E', '1', '1', '1'
+);
+
+INSERT INTO accounts_updated(
+account_id, change_type, contacts, contacts_group, hosts
+) VALUES (
+'2', 'E', '1', '0', '1'
+);
+
+INSERT INTO accounts_updated(
+account_id, change_type, contacts, contacts_group, hosts
+) VALUES (
+'1', 'E', '1', '1', '1'
+);
+
+INSERT INTO accounts_updated(
+account_id, change_type, contacts, contacts_group, hosts
+) VALUES (
+'2', 'E', '1', '0', '1'
+);
+
+INSERT INTO accounts_updated(
+account_id, change_type, contacts, contacts_group, hosts
+) VALUES (
+'1', 'E', '1', '1', '1'
+);
+
+INSERT INTO accounts_updated(
+account_id, change_type, contacts, contacts_group, hosts
+) VALUES (
+'2', 'E', '1', '1', '0'
+);
+
+INSERT INTO accounts_updated(
+account_id, change_type, contacts, contacts_group, hosts
+) VALUES (
+'1', 'E', '1', '0', '1'
+);
+
+INSERT INTO accounts_updated(
+account_id, change_type, contacts, contacts_group, hosts
+) VALUES (
+'2', 'E', '1', '1', '1'
+);
